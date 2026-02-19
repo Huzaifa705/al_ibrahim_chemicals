f5cec7358ee8f721b7e2586fa7c90a1fea33dbe2
Sohaib-Ur-Rehman01 <sohaiburrehman65@gmail.com>
2026-02-19 12:05:18 -0800

    logo size update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -119,7 +119,7 @@ const Navbar = () => {
                   alt="Al-Ibrahim Chemicals Logo"
                   width={100}
                   height={100}
-                  className="w-50 h-32"
+                  className="w-40 h-25"
                 />
               </div>
             </Link>
